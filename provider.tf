019238bad938f6d9e06454d4a164ee91a8791c49
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-09-12 11:08:43 +1200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.1.0"
+      version = "~> 3.0" # Adjust to the version you need
     }
   }
 }
 
 provider "azurerm" {
-  features {}  
-
+  features {}
+  version = "~> 3.0" # Adjust to the version you need
 }

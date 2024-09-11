7adbf12ab8cbed9d9493cdc21c6a8eb6cb0c0253
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-09-12 09:21:50 +1200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,3 +18,17 @@ variable "search_service" {
   description = "The name of the Azure Search Service"
   type        = string
 }
+
+variable "cognitive_account" {
+  description = "The name of the Azure Search Service"
+  type        = string
+}
+
+
+variable "sku" {
+  type        = string
+  description = "The sku name of the Azure Analysis Services server to create. Choose from: B1, B2, D1, S0, S1, S2, S3, S4, S8, S9. Some skus are region specific. See https://docs.microsoft.com/en-us/azure/analysis-services/analysis-services-overview#availability-by-region"
+ 
+}
+
+

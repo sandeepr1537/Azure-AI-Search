57a41a62e1b4be67317b0066a1a67dc416c68bc3
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-09-12 11:26:49 +1200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,4 +9,10 @@ terraform {
 
 provider "azurerm" {
  features{}
+
+ # These values should be set using environment variables
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
 }
